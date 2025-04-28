e4ccad286e541447516806f85fb256339cf73c12
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2025-04-28 19:18:19 +0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
             toast.remove();
         });
     }
-
+    
     const conditions = [
         'blinded',
         'charmed',
